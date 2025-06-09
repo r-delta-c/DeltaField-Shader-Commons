5134c8b57ef31f6148a076f3a54bcc082cfb5615
delta <derutadaikideruta@yahoo.co.jp>
2025-06-09 23:23:30 +0900

    dev-1.0.0

---
+++ Includes/macro_stereo_merge.hlsl
@@ -0,0 +1,19 @@
+#if defined(USING_STEREO_MATRICES)
+    #ifdef _STEREOMERGEMODE_POSITION
+        #define DEFINE_STEREO_MERGE_MATRIX_V BuildObjectMatrix(unity_MatrixV,(unity_StereoMatrixV[0]._m03_m13_m23+unity_StereoMatrixV[1]._m03_m13_m23)*0.5)
+
+    #elif _STEREOMERGEMODE_ROTATION
+        #define DEFINE_STEREO_MERGE_MATRIX_V BuildObjectMatrix(RMatAverage((float3x3)unity_StereoMatrixV[0],(float3x3)unity_StereoMatrixV[1]),(unity_MatrixV._m03_m13_m23))
+
+    #elif _STEREOMERGEMODE_POSITION_ROTATION
+        #define DEFINE_STEREO_MERGE_MATRIX_V BuildObjectMatrix(RMatAverage((float3x3)unity_StereoMatrixV[0],(float3x3)unity_StereoMatrixV[1]),(unity_StereoMatrixV[0]._m03_m13_m23+unity_StereoMatrixV[1]._m03_m13_m23)*0.5)
+
+    #else
+        #define DEFINE_STEREO_MERGE_MATRIX_V unity_MatrixV
+    #endif
+
+    #define CAMERA_DISTANCE_MACRO length((unity_StereoWorldSpaceCameraPos[0]+unity_StereoWorldSpaceCameraPos[1])*0.5-o.vertex)
+#else
+    #define DEFINE_STEREO_MERGE_MATRIX_V unity_MatrixV
+    #define CAMERA_DISTANCE_MACRO length(_WorldSpaceCameraPos-o.vertex)
+#endif
--- Includes/vertex_billboard.hlsl
+++ Includes/vertex_billboard.hlsl
@@ -1,4 +1,4 @@
-float4x4 Stereo_Average_Matrix_V = DEFINE_STEREO_AVERAGE_MATRIX_V;
+float4x4 Stereo_Merge_Matrix_V = DEFINE_STEREO_MERGE_MATRIX_V;
 float4x4 Billboard_Matrix_M = {
     1.0,0.0,0.0,0.0,
     0.0,1.0,0.0,0.0,
@@ -6,11 +6,11 @@ float4x4 Billboard_Matrix_M = {
     0.0,0.0,0.0,1.0
 };
 
-Billboard_Matrix_M._m00_m10_m20 = Stereo_Average_Matrix_V[0].xyz*length(unity_ObjectToWorld._m00_m10_m20);
-Billboard_Matrix_M._m01_m11_m21 = Stereo_Average_Matrix_V[1].xyz*length(unity_ObjectToWorld._m01_m11_m21);
-Billboard_Matrix_M._m02_m12_m22 = -Stereo_Average_Matrix_V[2].xyz*length(unity_ObjectToWorld._m02_m12_m22)*(1.0-_Forced_Z_Scale_Zero);
+Billboard_Matrix_M._m00_m10_m20 = Stereo_Merge_Matrix_V[0].xyz*length(unity_ObjectToWorld._m00_m10_m20);
+Billboard_Matrix_M._m01_m11_m21 = Stereo_Merge_Matrix_V[1].xyz*length(unity_ObjectToWorld._m01_m11_m21);
+Billboard_Matrix_M._m02_m12_m22 = -Stereo_Merge_Matrix_V[2].xyz*length(unity_ObjectToWorld._m02_m12_m22)*(1.0-_Forced_Z_Scale_Zero);
 Billboard_Matrix_M._m03_m13_m23 = unity_ObjectToWorld._m03_m13_m23;
 
 o.vertex = mul(Billboard_Matrix_M,v.vertex);
-o.vertex = mul(Stereo_Average_Matrix_V,o.vertex);
+o.vertex = mul(Stereo_Merge_Matrix_V,o.vertex);
 o.vertex = mul(glstate_matrix_projection,o.vertex);
